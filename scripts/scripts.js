39e04cd1e4c5565693229c7412575e18475b46ae
Alex Gaytsuk <gayrsuk@gmail.com>
2020-08-09 23:48:38 +0300

    add cells in field, move cells to zero position, random position in numcells, but one of them undefined

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,71 +1,36 @@
 const field = document.getElementById('field');
 
-const range = (start, stop, step) => Array.from({length: (stop - start) / step + 1}, (_, i) => start + (i * step));
-
 const cellSize = 100;
 
 const empty = {
     top: 0,
     left: 0,
-    value: 0,
 };
 
-const cells = [];
-cells.push(empty);
-
-const numbers = range(1, 15, 1);
-
-console.log(numbers.sort(() => Math.random() - 0.5));
-
-const numbers1 = [...Array(15).keys()]
+const cells = [...Array(15).keys()]
     .map(x => x + 1)
     .sort(() => Math.random() - 0.5);
 
-console.log(numbers1);
-
-const numbers2 = Array.from()
-
-const move = index => {
-    const cell = cells[index];
-    const leftDiff = Math.abs(empty.left - cell.left);
-    const topDiff = Math.abs(empty.top - cell.top);
-
-    if (leftDiff + topDiff > 1) return;
-
-
-    cell.element.style.top = `${empty.top * cellSize}px`;
-    cell.element.style.left = `${empty.left * cellSize}px`;
-
-    const emptyLeft = empty.left;
-    const emptyTop = empty.top;
-
-    empty.left = cell.left;
-    empty.top = cell.top;
-    cell.left = emptyLeft;
-    cell.top = emptyTop;
-
-};
+console.log(cells);
 
 for (let i = 1; i <= 15; i++) {
+
     const cell = document.createElement('div');
+    cell.innerText = cells[i];
     cell.className = 'cell';
-    cell.innerHTML = i;
+
 
     const left = i % 4;
     const top = (i - left) / 4;
 
-    cells.push({
-        left: left,
-        top: top,
-        element: cell,
-    });
-
     cell.style.top = `${top * cellSize}px`;
     cell.style.left = `${left * cellSize}px`;
+
 
     field.append(cell);
 
     cell.addEventListener('click', () => {
-        move(i)
+        cell.style.top = `${empty.top * cellSize}px`;
+        cell.style.left = `${empty.left * cellSize}px`;
     })
 }
